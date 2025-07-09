22cee1bd4ef14f9ba40cece7cbc34173929b5014
michaelfried-dev <email@michaelfried.info>
2025-07-09 15:04:22 -0400

    Formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html suppressHydrationWarning lang="en">
       <body className={archivo.className}>
         <ThemeProvider attribute="class" disableTransitionOnChange>
-          <div className="outline-border rounded-base w600:grid-cols-[70px_auto] w500:grid-cols-1 grid h-[800px] max-h-[100dvh] w-[1000px] max-w-[1000px] grid-cols-[100px_auto] shadow-[10px_10px_0_0_#000] outline-4 portrait:h-[100dvh]">
+          <div className="outline-border rounded-base w600:grid-cols-[70px_auto] w500:grid-cols-1 grid h-[800px] max-h-[100dvh] w-[1000px] max-w-[1000px] grid-cols-[100px_auto] shadow-[10px_10px_0_0_#000] outline-4 portrait:h-[100dvh] portrait:w1000:max-h-[800px] mdHeight:w-[100dvw] mdHeight:max-w-[100dvw]">
             <header>
               <Link href="/" className="border-r-border rounded-l-base bg-main w500:hidden relative flex items-center justify-center border-r-4 portrait:rounded-none h-full">
                 <h1 className="smallHeight:text-[30px] smallHeight:tracking-[2px] w600:text-[30px] w600:tracking-[2px] -rotate-90 text-[40px] font-bold tracking-[4px] whitespace-nowrap">
@@ -33,9 +33,9 @@ export default function RootLayout({
                 </h1>
               </Link>
             </header>
-            <main className="rounded-br-base rounded-tr-base bg-background relative flex h-[800px] max-h-[100dvh] flex-col font-semibold portrait:h-[100dvh] portrait:max-h-[100dvh] portrait:rounded-none">
+            <main className="rounded-br-base rounded-tr-base bg-background relative flex h-[800px] max-h-[100dvh] flex-col font-semibold portrait:h-[100dvh] portrait:max-h-[100dvh] portrait:w1000:h-[800px] portrait:rounded-none">
               <Nav />
-              <div className="main h-full max-h-[750px] overflow-y-auto portrait:max-h-[calc(100dvh-50px)]">
+              <div className="main h-full max-h-[750px] overflow-y-auto portrait:max-h-[calc(100dvh-50px)] portrait:w1000:h-[800px]">
                 {children}
               </div>
             </main>
